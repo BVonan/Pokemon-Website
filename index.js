d769f0bc5c8c775b15c714ce850fc1ebdb09742c
BVonan <bohnny14@gmail.com>
2023-04-05 01:43:35 -0700

    New pokemon appears everytime the page is reloaded

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const express = require('express');
+const pokemon = require('pokemon');
 const app = express();
-app.set("view engine", "ejs");
-app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-   res.render('home')
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', async (req, res) => {
+  const randomPokemon = pokemon.random();
+  const pokeApiUrl = `https://pokeapi.co/api/v2/pokemon/${randomPokemon.toLowerCase()}`;
+
+  try {
+    const pokeApiResponse = await fetch(pokeApiUrl);
+    const pokemonData = await pokeApiResponse.json();
+    const pokemonImgUrl = pokemonData.sprites.front_default;
+
+    res.render('home', {
+      randomPokemon,
+      pokemonImgUrl,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error fetching pokemon data');
+  }
 });
-
-// find a pokemon api
-
 
 app.listen(3000, () => {
-   console.log('server started');
+  console.log('Server started');
 });
+
+
+
+// the plan:
+// generate a random poekmon
+// the next page shows its type (user types it in)
+// the next page shows its region (user types it in)
+// the next page shows its strengths and weakness (user types it in)
+
