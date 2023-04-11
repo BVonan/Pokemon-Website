0089ee3c6bf32b2cc1caf2c789153087bbc6c0ae
BVonan <bohnny14@gmail.com>
2023-04-11 12:12:20 -0700

    updated and cleaned code

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 ----------------------------Random search----------------------------------
 */
 
+
 app.get('/', async (req, res) => {
   try {
     const randomPokemon = pokemon.random();
